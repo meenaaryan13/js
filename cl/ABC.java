94e9a42e3261c39f742cfde7d0aef11c817fa2fe
meenaaryan13 <meenaaryan771@gmail.com>
2022-06-24 14:13:19 +0530

    first commit

---
+++ cl/ABC.java
@@ -0,0 +1,23 @@
+package cl;
+public class ABC {
+  public static void main(String[] args) {
+    int a[] = { 10, 20, 30, 21 };
+    int n = a.length,j;
+    for (int i = 0; i < n; i++) {
+      // System.out.print(a[i]);
+      if (a[i] % 2 == 0) {
+        for (j = i + 1; j < n; j++) {
+          a[i] = a[j];
+          System.out.print(a[j]);
+        }
+        i--;
+        n--;
+        j--;
+        // System.out.print(n);
+      }
+    }
+    for (j = 0; j < 7; j++) {
+      // System.out.println(a[j] + " ");
+    }
+  }
+}
---
+++ cl/Evenod.java
@@ -0,0 +1,19 @@
+package cl;
+class Evenod {
+  public static void main(String[] args) {
+    int a[]={2,1,4,5,6};
+    int n=a.length;
+    for(int i=0;i<n;i++)
+    {
+      if(a[i]%2==0)
+      {
+        System.out.print(a[i]);
+      }
+      else
+      {
+        System.out.print(a[i]);
+      }
+    }
+  }
+  
+}
